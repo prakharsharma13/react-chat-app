0f6f696a82a8c436e96f84074ab83c465bbf1835
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-04-27 09:13:53 +0530

    Search bar design

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import "./chatList.css"
+import React, { useState } from "react";
+import "./chatList.css";
 
 const ChatList = () => {
+  const [addMode, setAddMode] = useState(false);
   return (
-    <div className='chatList'>ChatList</div>
-  )
-}
+    <div className="chatList">
+      <div className="search">
+        <div className="searchBar">
+          <img src="/search.png" alt="" />
+          <input type="text" placeholder="Search..." />
+        </div>
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+          className="add"
+          onClick={() => setAddMode((prev) => !prev)}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default ChatList
+export default ChatList;
