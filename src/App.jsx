67c5c6a097d9beade4453b001272a7783a53db50
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-05-04 01:10:23 +0530

    React Login Page Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import Chat from "./components/chat/Chat"
-import Detail from "./components/detail/Detail"
-import List from "./components/list/List"
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+import List from "./components/list/List";
+import Login from "./components/Login/Login";
 
 const App = () => {
+  const user = false;
   return (
-    <div className='container'>
-      <List />
-      <Chat />
-      <Detail />
+    <div className="container">
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
