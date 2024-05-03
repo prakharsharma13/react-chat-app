0956c3aaf0aef1c156ef77675cb7ceace5086995
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-05-03 08:10:01 +0530

    chat detail component design

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,10 +1,90 @@
-import React from 'react';
-import "./detail.css"
+import React from "react";
+import "./detail.css";
 
 const Detail = () => {
   return (
-    <div className='detail'>Detail</div>
-  )
-}
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jane Doe</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.unsplash.com/photo-1714317809096-13f673467c67?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt=""
+                />
+                <span>photo_24_2.png</span>
+              </div>
 
-export default Detail
+              <img src="./download.png" className="icon" alt="" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.unsplash.com/photo-1714317809096-13f673467c67?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt=""
+                />
+                <span>photo_24_2.png</span>
+              </div>
+
+              <img src="./download.png" className="icon" alt="" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.unsplash.com/photo-1714317809096-13f673467c67?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt=""
+                />
+                <span>photo_24_2.png</span>
+              </div>
+
+              <img src="./download.png" className="icon" alt="" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.unsplash.com/photo-1714317809096-13f673467c67?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt=""
+                />
+                <span>photo_24_2.png</span>
+              </div>
+
+              <img src="./download.png" className="icon" alt="" />
+            </div>
+           
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
+  );
+};
+
+export default Detail;
