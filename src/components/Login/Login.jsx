e296e8d4b31ea0209da364d9fa566b6e1e4d23cf
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-05-05 09:30:53 +0530

    React firebase Signup with email and password

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./login.css";
 import { toast } from "react-toastify";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../lib/firebase";
 
 const Login = () => {
   const [avatar, setAvatar] = useState({
@@ -17,13 +19,19 @@ const Login = () => {
     }
   };
 
-  const handleRegister = e => {
+  const handleRegister = async  e => {
     e.preventDefault();
     const formData = new FormData(e.target);
 
     const {username, email, password} = Object.fromEntries(formData);
 
-    console.log(username)
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email,password)
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    }
+
   }
 
   const handleLogin = e => {
