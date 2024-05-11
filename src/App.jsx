69b47f0f78aee59b131346fc97db3542b61bd67e
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-05-11 09:57:55 +0530

    storing user after authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { useEffect } from "react";
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import List from "./components/list/List";
 import Login from "./components/Login/Login";
 import Notification from "./components/Notification/Notification";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
 
 const App = () => {
   const user = false;
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      console.log(user)
+    });
+
+    return () => {
+      unSub();
+    }
+  }, [])
   return (
     <div className="container">
       {user ? (
