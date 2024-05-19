16d0900b79befe18a9e4138b7e6996711af8986e
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-05-19 09:32:52 +0530

    React firebase auth with zustand state management tool

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,26 @@ import Login from "./components/Login/Login";
 import Notification from "./components/Notification/Notification";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userStore";
 
 const App = () => {
-  const user = false;
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
 
   useEffect(() => {
     const unSub = onAuthStateChanged(auth, (user) => {
-      console.log(user)
+      fetchUserInfo(user?.uid);
     });
 
     return () => {
       unSub();
-    }
-  }, [])
+    };
+  }, [fetchUserInfo]);
+
+
+  if (isLoading) return <div className="loading">Loading!</div>;
   return (
     <div className="container">
-      {user ? (
+      {currentUser ? (
         <>
           <List />
           <Chat />
